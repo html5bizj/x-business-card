be83c1c2fee246642d578b11673e1d53bc2674b5
Mitsuru Ogawa <mitsuru.ogawa.jp@gmail.com>
2014-08-27 03:47:11 +0000

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,11 +11,11 @@ module.exports = function(grunt) {
         },
         'gh-pages': {
             options: {
-                clone: 'bower_components/my-repo'
+                clone: 'bower_components/x-business-card'
             },
             src: [
                 'bower_components/**/*',
-                '!bower_components/my-repo/**/*',
+                '!bower_components/x-business-card/**/*',
                 'demo/*', 'src/*', 'index.html'
             ]
         }
